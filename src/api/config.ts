677dfe3d26c7b4cbbf6676cd0575e76814d46822
up-11 <iurykolgin@gmail.com>
2025-04-15 02:11:51 +0700

    config fix

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
 export const API = {
-  BASE_URL: 'nometech-backend-0hvyin-87d2dd-195-133-194-39.traefik.me',
+  BASE_URL: 'http://nometech-backend-0hvyin-87d2dd-195-133-194-39.traefik.me',
   AUTH: {
     LOGIN: 'authorization/sign-in',
   },
